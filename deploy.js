cf847d3a254f18b74ba8779e747cc5c3cd48d464
lvlindav <lvlindav23@gmail.com>
2021-02-27 22:11:01 -0600

    modified deploy.js

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '100.25.193.38',
+      host: '3.85.177.239',
       username: 'ubuntu',
       privateKey: 'start-node-key.pem'
     })
